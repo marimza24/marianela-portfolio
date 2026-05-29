0ef0ffcbb3cd57fe48e7609472cd92a430c0a090
marimza24 <malemza24@hotmail.com>
2026-05-29 10:30:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,109 +1,108 @@
 // VIDEOS
 
 const videos = [
-  "https://www.youtube.com/embed/A0DOnZBaeew",
-  "https://www.youtube.com/embed/TU_SEGUNDO_VIDEO",
-  "https://www.youtube.com/embed/TU_TERCER_VIDEO"
+  "A0DOnZBaeew",
+  "1_ttidViLvo",
+  "Oe3F9LMyWNg",
+  "t577JMNsdhM"
 ];
 
 let currentVideo = 0;
 
+function showVideo() {
+  const frame = document.getElementById("videoFrame");
+
+  if (frame) {
+    frame.src = `https://www.youtube.com/embed/${videos[currentVideo]}`;
+  }
+}
+
 function nextVideo() {
-  currentVideo = (currentVideo + 1) % videos.length;
-  document.getElementById("videoFrame").src =
-    videos[currentVideo];
+  currentVideo++;
+
+  if (currentVideo >= videos.length) {
+    currentVideo = 0;
+  }
+
+  showVideo();
 }
 
 function previousVideo() {
-  currentVideo =
-    (currentVideo - 1 + videos.length) %
-    videos.length;
+  currentVideo--;
 
-  document.getElementById("videoFrame").src =
-    videos[currentVideo];
+  if (currentVideo < 0) {
+    currentVideo = videos.length - 1;
+  }
+
+  showVideo();
 }
 
 
 // KOREA.NET
 
 const articles = [
-
-{
-image: "series motivacionales.jpeg",
-title:
-"4 series coreanas que te motivan a luchar por tus sueños",
-link:
-"https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=225686"
-},
-
-{
-image: "comida coreana.png",
-title:
-"Gastronomía coreana",
-link:
-"https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=211684"
-},
-
-{
-image: "los 90.png",
-title:
-"Corea y los años 90",
-link:
-"https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=193436"
-},
-
-{
-image: "danyang.png",
-title:
-"Danyang",
-link:
-"https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=186084"
-},
-
-{
-image: "uleungdo.png",
-title:
-"Ulleungdo",
-link:
-"https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=187808"
-},
-
-{
-image: "dramas histori.png",
-title:
-"Dramas históricos",
-link:
-"https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=197035"
-}
-
+  {
+    image: "series motivacionales.jpeg",
+    title: "4 series coreanas que te motivan a luchar por tus sueños",
+    link: "https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=197035"
+  },
+  {
+    image: "los 90.png",
+    title: "4 series coreanas que te harán viajar a los años 90",
+    link: "https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=225686"
+  },
+  {
+    image: "comida coreana.png",
+    title: "Comida coreana: tradición, sabor y cultura",
+    link: "https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=211684"
+  },
+  {
+    image: "danyang.png",
+    title: "Danyang",
+    link: "https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=186084"
+  },
+  {
+    image: "uleungdo.png",
+    title: "Ulleungdo",
+    link: "https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=187808"
+  },
+  {
+    image: "dramas histori.png",
+    title: "Dramas históricos coreanos",
+    link: "https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=193436"
+  }
 ];
 
 let currentArticle = 0;
 
-function updateArticle() {
+function showArticle() {
+  const image = document.getElementById("koreaFrame");
+  const title = document.getElementById("koreaTitle");
+  const link = document.getElementById("koreaLink");
 
-document.getElementById("koreaFrame").src =
-  articles[currentArticle].image;
-
-document.getElementById("koreaTitle").textContent =
-  articles[currentArticle].title;
-
-document.getElementById("koreaLink").href =
-  articles[currentArticle].link;
+  if (image && title && link) {
+    image.src = articles[currentArticle].image;
+    title.textContent = articles[currentArticle].title;
+    link.href = articles[currentArticle].link;
+  }
 }
 
 function nextArticle() {
-currentArticle =
-(currentArticle + 1) %
-articles.length;
+  currentArticle++;
 
-updateArticle();
+  if (currentArticle >= articles.length) {
+    currentArticle = 0;
+  }
+
+  showArticle();
 }
 
 function previousArticle() {
-currentArticle =
-(currentArticle - 1 + articles.length) %
-articles.length;
+  currentArticle--;
 
-updateArticle();
+  if (currentArticle < 0) {
+    currentArticle = articles.length - 1;
+  }
+
+  showArticle();
 }
