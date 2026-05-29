9a50ccdbe6fcfec257dfd8e9ea77996014232148
marimza24 <malemza24@hotmail.com>
2026-05-29 10:24:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,109 @@
+// VIDEOS
+
 const videos = [
-  "A0DOnZBaeew",
-  "1_ttidViLvo",
-  "Oe3F9LMyWNg",
-  "t577JMNsdhM"
+  "https://www.youtube.com/embed/A0DOnZBaeew",
+  "https://www.youtube.com/embed/TU_SEGUNDO_VIDEO",
+  "https://www.youtube.com/embed/TU_TERCER_VIDEO"
 ];
 
 let currentVideo = 0;
 
-function showVideo() {
-  const frame = document.getElementById("videoFrame");
-  frame.src = `https://www.youtube.com/embed/${videos[currentVideo]}`;
-}
-
 function nextVideo() {
-  currentVideo++;
-
-  if (currentVideo >= videos.length) {
-    currentVideo = 0;
-  }
-
-  showVideo();
+  currentVideo = (currentVideo + 1) % videos.length;
+  document.getElementById("videoFrame").src =
+    videos[currentVideo];
 }
 
 function previousVideo() {
-  currentVideo--;
+  currentVideo =
+    (currentVideo - 1 + videos.length) %
+    videos.length;
 
-  if (currentVideo < 0) {
-    currentVideo = videos.length - 1;
-  }
+  document.getElementById("videoFrame").src =
+    videos[currentVideo];
+}
 
-  showVideo();
+
+// KOREA.NET
+
+const articles = [
+
+{
+image: "series motivacionales.jpeg",
+title:
+"4 series coreanas que te motivan a luchar por tus sueños",
+link:
+"https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=225686"
+},
+
+{
+image: "comida coreana.png",
+title:
+"Gastronomía coreana",
+link:
+"https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=211684"
+},
+
+{
+image: "los 90.png",
+title:
+"Corea y los años 90",
+link:
+"https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=193436"
+},
+
+{
+image: "danyang.png",
+title:
+"Danyang",
+link:
+"https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=186084"
+},
+
+{
+image: "uleungdo.png",
+title:
+"Ulleungdo",
+link:
+"https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=187808"
+},
+
+{
+image: "dramas histori.png",
+title:
+"Dramas históricos",
+link:
+"https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=197035"
+}
+
+];
+
+let currentArticle = 0;
+
+function updateArticle() {
+
+document.getElementById("koreaFrame").src =
+  articles[currentArticle].image;
+
+document.getElementById("koreaTitle").textContent =
+  articles[currentArticle].title;
+
+document.getElementById("koreaLink").href =
+  articles[currentArticle].link;
+}
+
+function nextArticle() {
+currentArticle =
+(currentArticle + 1) %
+articles.length;
+
+updateArticle();
+}
+
+function previousArticle() {
+currentArticle =
+(currentArticle - 1 + articles.length) %
+articles.length;
+
+updateArticle();
 }
