5fcf6bbf5c93cd9f62fa693479267e139c293edf
marimza24 <malemza24@hotmail.com>
2026-05-29 11:12:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,103 @@
+// VIDEOS
+
+const videos = [
+  "A0DOnZBaeew",
+  "1_ttidViLvo",
+  "Oe3F9LMyWNg",
+  "t577JMNsdhM"
+];
+
+let currentVideo = 0;
+
 function nextVideo() {
-  alert("Botón de video funciona");
+  currentVideo++;
+
+  if (currentVideo >= videos.length) {
+    currentVideo = 0;
+  }
+
+  document.getElementById("videoFrame").src =
+    "https://www.youtube.com/embed/" + videos[currentVideo];
 }
 
 function previousVideo() {
-  alert("Botón de video funciona");
+  currentVideo--;
+
+  if (currentVideo < 0) {
+    currentVideo = videos.length - 1;
+  }
+
+  document.getElementById("videoFrame").src =
+    "https://www.youtube.com/embed/" + videos[currentVideo];
+}
+
+
+// KOREA.NET
+
+const articles = [
+  {
+    image: "series motivacionales.jpeg",
+    title: "4 series coreanas que te motivan a luchar por tus sueños",
+    link: "https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=197035"
+  },
+  {
+    image: "los 90.png",
+    title: "Volviendo a los años 90",
+    link: "https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=225686"
+  },
+  {
+    image: "comida coreana.png",
+    title: "Comida saludable, comida coreana",
+    link: "https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=193436"
+  },
+  {
+    image: "danyang.png",
+    title: "En el corazón de Corea: Danyang",
+    link: "https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=187808"
+  },
+  {
+    image: "uleungdo.png",
+    title: "Ulleungdo: una misteriosa isla en el Mar del Este",
+    link: "https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=211684"
+  },
+  {
+    image: "dramas histori.png",
+    title: "Una mirada diferente para disfrutar dramas históricos",
+    link: "https://spanish.korea.net/NewsFocus/HonoraryReporters/view?articleId=186084"
+  }
+];
+
+let currentArticle = 0;
+
+function updateArticle() {
+  document.getElementById("koreaFrame").src =
+    articles[currentArticle].image;
+
+  document.getElementById("koreaTitle").textContent =
+    articles[currentArticle].title;
+
+  document.getElementById("koreaLink").href =
+    articles[currentArticle].link;
 }
 
 function nextArticle() {
-  alert("Botón de artículo funciona");
+  currentArticle++;
+
+  if (currentArticle >= articles.length) {
+    currentArticle = 0;
+  }
+
+  updateArticle();
 }
 
 function previousArticle() {
-  alert("Botón de artículo funciona");
+  currentArticle--;
+
+  if (currentArticle < 0) {
+    currentArticle = articles.length - 1;
+  }
+
+  updateArticle();
 }
+
+updateArticle();
